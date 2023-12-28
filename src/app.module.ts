c7f19c7c16649e588756022b6c37e99fa9976974
Em1lT <emilanton.toivainen@gmail.com>
2023-12-29 00:18:14 +0200

    feat: sql integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TasksService } from './tasks/task.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [],
-  providers: [TasksService],
+  providers: [TasksService, PrismaService],
 })
 export class AppModule {}
--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,34 +1,55 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from '../prisma.service';
 import cheerio from 'cheerio';
-
-type PriceEntity = {
-    price: string;
-    from: string;
-    to: string;
-}
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
+  constructor(private prisma: PrismaService) {}
 
   @Cron(CronExpression.EVERY_10_SECONDS)
   async handleCron() {
     this.logger.debug('fetch new price of the electricity');
-    const URL = 'https://www.porssisahkoa.fi/'
-    const s = await fetch(URL);
-    const $ = cheerio.load(await s.text());
-    const $p = $('div > div > h1');
-    const allChildren = $p.parent().children();
+    const URL = 'https://www.porssisahkoa.fi/';
+    const pageHtml = await fetch(URL);
+    const $ = cheerio.load(await pageHtml.text());
+    const $priceBanner = $('div > div > h1');
+    const allChildren = $priceBanner.parent().children();
     const text = allChildren.map((_, item) => $(item).text()).get();
 
-    const [from, to] = text[1].split(' - ').map(i => i.trim());
+    const [from, to] = text[1].split(' - ').map((i) => i.trim());
 
-    const price: PriceEntity = {
-      price: text[2],
-      from: new Date().setHours(+!from.split(' ').at(0) as number).toString(),
-      to: new Date().setHours(+to).toString()
+    const priceObject = {
+      price: +text[2].replace(',', '.'),
+      // change from to ISO-8601 DateTime
+      from: new Date(
+        new Date().setHours(+!from.split(' ').at(0) as number),
+      ).toISOString(),
+      to: new Date(new Date().setHours(+to)).toISOString(),
+    };
+
+    const alreadySet = await this.prisma.price.findFirst({
+      where: {
+        price: priceObject.price,
+        AND: [
+          {
+            to: { lte: priceObject.to },
+            from: { gte: priceObject.from },
+          },
+        ],
+      },
+    });
+
+    if (alreadySet) {
+      this.logger.log('Already set this price', priceObject);
+      return;
     }
-    this.logger.log(price)
+
+    this.logger.log('Saving new price of the electricity', priceObject);
+    await this.prisma.price.create({
+      data: priceObject,
+    });
+    return;
   }
 }
