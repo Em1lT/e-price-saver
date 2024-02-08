f0123235f4a9eab1f7f4b1a75f4a72669409c8fc
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-02-08 22:13:32 +0200

    feat: new cron job

--- src/tasks/task.service.ts
+++ src/tasks/task.service.ts
@@ -1,33 +1,40 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import axios from 'axios';
-import cheerio from 'cheerio';
+import { format, addHours } from 'date-fns';
 import { TelegramService } from 'nestjs-telegram';
+
+interface PriceObject {
+  time: string;
+  value: number;
+}
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
   constructor(private readonly telegram: TelegramService) {}
 
-  // @Cron('1 7-21 * * *')
-  @Cron('* * * * *')
+  @Cron('1 7-21 * * *')
+  // @Cron('* * * * *')
   async saveElectricityPrice() {
     this.logger.debug('Fetch new price of the electricity', new Date());
-    const URL = process.env.ELECTRICITY_PRICE_URL;
-    const pageHtml = await axios.get(URL);
-    const $ = cheerio.load(await pageHtml.data);
-    const $priceBanner = $('div > div > h1');
-    const allChildren = $priceBanner.parent().children();
-    const text = allChildren.map((_, item) => $(item).text()).get();
+    const URL = `${process.env.ELECTRICITY_PRICE_URL}/api/Prices/GetPrices?mode=1`;
+    const response = await axios.get(URL);
+    const data = response.data;
+    const currentHour = format(Date.now(), 'H');
 
-    const [from, to] = text[1].split(' - ').map((i) => i.trim());
-    const fromHour = +from.split(' ').at(1) as number;
+    const price = data.find(
+      (price: PriceObject) => format(price.time, 'H') === currentHour,
+    );
+    const dateTime = new Date(price.time);
 
     const priceObject = {
-      price: +text[2].replace(',', '.'),
-      from: new Date(new Date().setHours(fromHour)).toISOString(),
-      to: new Date(new Date().setHours(+to)).toISOString(),
+      price: price.value,
+      from: dateTime.toISOString(), // new Date(new Date().setHours(fromHour)).toISOString(),
+      to: new Date(addHours(dateTime, 1)).toISOString(), // new Date(new Date().setHours(fromHour)).toISOString(),
     };
+
+    console.log(priceObject);
 
     // const alreadySet = await this.prisma.price.findFirst({
     //   where: {
@@ -58,9 +65,175 @@ export class TasksService {
     await this.telegram
       .sendMessage({
         chat_id: process.env.TELEGRAM_CHAT_ID,
-        text: `Hinta nyt: ${priceObject.price} Senttiä`,
+        text: `Hinta nyt: ${priceObject.price} Snt/kwh`,
       })
       .toPromise();
     return;
   }
+
+  @Cron('* * * * *')
+  async getTomorrowEPrices() {
+    this.logger.debug('Fetch new price of the electricity', new Date());
+    const URL = `${process.env.ELECTRICITY_PRICE_URL}/api/Prices/GetPrices?mode=2`;
+    const response = await axios.get(URL);
+    const data = response.data;
+
+    if (data.length < 5) {
+      this.logger.debug('Prices not yet updated', new Date());
+    }
+
+    const tomorrowPrices = data.map((item: PriceObject) => {
+      const dateTime = new Date(item.time);
+      return {
+        price: item.value,
+        from: dateTime.toISOString(), // new Date(new Date().setHours(fromHour)).toISOString(),
+        to: new Date(addHours(dateTime, 1)).toISOString(), // new Date(new Date().setHours(fromHour)).toISOString(),
+      };
+    });
+
+    // check this!!!!
+    const text = tomorrowPrices
+      .map(
+        (item: any) =>
+          `Klo ${format(item.from, 'HH:mm')} - ${item.price} Snt/kwh \n`,
+      )
+      .join(',')
+      .replaceAll(',', '');
+
+    const sortedPrices = tomorrowPrices.sort((a, b) => {
+      return a.price - b.price;
+    });
+    const lowest = sortedPrices.shift();
+    const highest = sortedPrices.pop();
+
+    const t1 = `
+Tomorrows info! ⚡
+  highest: ${highest.price}
+  lowest: ${lowest.price}
+${text}
+      `;
+    console.log(t1);
+    await this.telegram
+      .sendMessage({
+        chat_id: process.env.TELEGRAM_CHAT_ID,
+        text: `
+        Tomorrows info! ⚡
+        highest: ${highest}
+        lowest: ${lowest}
+        `,
+      })
+      .toPromise();
+
+    return;
+  }
 }
+
+/* 
+
+// if no tomorrows prices
+GET https://www.porssisahkoa.fi/api/Prices/GetPrices?mode=2
+[{"time":"2024-02-09T00:00:00+02:00","value":12.40}]
+ 
+
+if working correctly
+GET https://www.porssisahkoa.fi/api/Prices/GetPrices?mode=1
+[
+    {
+        "time": "2024-02-08T00:00:00+02:00",
+        "value": 11.79
+    },
+    {
+        "time": "2024-02-08T01:00:00+02:00",
+        "value": 12.69
+    },
+    {
+        "time": "2024-02-08T02:00:00+02:00",
+        "value": 11.03
+    },
+    {
+        "time": "2024-02-08T03:00:00+02:00",
+        "value": 9.92
+    },
+    {
+        "time": "2024-02-08T04:00:00+02:00",
+        "value": 8.95
+    },
+    {
+        "time": "2024-02-08T05:00:00+02:00",
+        "value": 11.10
+    },
+    {
+        "time": "2024-02-08T06:00:00+02:00",
+        "value": 16.56
+    },
+    {
+        "time": "2024-02-08T07:00:00+02:00",
+        "value": 16.71
+    },
+    {
+        "time": "2024-02-08T08:00:00+02:00",
+        "value": 16.32
+    },
+    {
+        "time": "2024-02-08T09:00:00+02:00",
+        "value": 16.59
+    },
+    {
+        "time": "2024-02-08T10:00:00+02:00",
+        "value": 14.08
+    },
+    {
+        "time": "2024-02-08T11:00:00+02:00",
+        "value": 12.13
+    },
+    {
+        "time": "2024-02-08T12:00:00+02:00",
+        "value": 11.39
+    },
+    {
+        "time": "2024-02-08T13:00:00+02:00",
+        "value": 12.50
+    },
+    {
+        "time": "2024-02-08T14:00:00+02:00",
+        "value": 15.31
+    },
+    {
+        "time": "2024-02-08T15:00:00+02:00",
+        "value": 15.72
+    },
+    {
+        "time": "2024-02-08T16:00:00+02:00",
+        "value": 16.12
+    },
+    {
+        "time": "2024-02-08T17:00:00+02:00",
+        "value": 15.30
+    },
+    {
+        "time": "2024-02-08T18:00:00+02:00",
+        "value": 16.47
+    },
+    {
+        "time": "2024-02-08T19:00:00+02:00",
+        "value": 15.83
+    },
+    {
+        "time": "2024-02-08T20:00:00+02:00",
+        "value": 15.86
+    },
+    {
+        "time": "2024-02-08T21:00:00+02:00",
+        "value": 14.93
+    },
+    {
+        "time": "2024-02-08T22:00:00+02:00",
+        "value": 13.97
+    },
+    {
+        "time": "2024-02-08T23:00:00+02:00",
+        "value": 12.60
+    }
+]
+
+ */
